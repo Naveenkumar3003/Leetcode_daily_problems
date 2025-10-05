851251cd8775456150ee8de77a2f6ef0da2b2ae5
Naveenkumar3003 <naveengoogl2006@gmail.com>
2025-10-05 18:51:15 +0530

    Updated lowestCommonAncestor.py

--- lowestCommonAncestor.py
+++ lowestCommonAncestor.py
@@ -1,1 +1,11 @@
+def lca(root, v1, v2):
+    if root is None:
+        return root
+    if v1==root.info or v2==root.info:
+        return root
+    left_lca=lca(root.left,v1,v2)
+    right_lca=lca(root.right,v1,v2)
+    if left_lca and right_lca:
+        return root
 
+    return left_lca if left_lca else right_lca
