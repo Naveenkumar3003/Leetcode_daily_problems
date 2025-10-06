59d4c57a693a1aa838b990f7fbc650ac3d758966
Naveenkumar3003 <naveengoogl2006@gmail.com>
2025-10-06 16:10:43 +0530

    Updated ReverseBits.cpp

--- ReverseBits.cpp
+++ ReverseBits.cpp
@@ -1,1 +1,11 @@
-
+class Solution {
+public:
+    int reverseBits(int n) {
+        long long res=0;
+        for(int i=0;i<32;i++){
+            long long bit=(n>>i)&1;
+            res=res|(bit<<(31-i));
+        }
+        return (int) res;
+    }
+};
