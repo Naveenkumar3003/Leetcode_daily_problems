2914dc0b1d9cbd61db999c5381be54d41877720e
Naveenkumar3003 <naveengoogl2006@gmail.com>
2025-11-01 09:46:00 +0530

    Add function to delete nodes from linked list
    Implement a solution to remove specified nodes from a linked list based on input values.

--- deleteNodesInArr.cpp
+++ deleteNodesInArr.cpp
@@ -1,1 +1,18 @@
-
+class Solution {
+public:
+    ListNode* modifiedList(vector<int>& nums, ListNode* head) {
+        unordered_set<int> numsTodel(nums.begin(),nums.end());
+        ListNode* dummy=new ListNode(0);
+        dummy->next=head;
+        ListNode* curr=dummy;
+        while(curr->next!=nullptr){
+            if(numsTodel.find(curr->next->val)!=numsTodel.end()){
+                curr->next=curr->next->next;
+            }
+            else{
+                curr=curr->next;
+            }
+        }
+        return dummy->next;
+    }
+};
