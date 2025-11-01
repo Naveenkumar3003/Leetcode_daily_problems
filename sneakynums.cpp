273bf01f5e56572760128c01be1a70814c1031b6
Naveenkumar3003 <naveengoogl2006@gmail.com>
2025-11-01 09:44:36 +0530

    Implement method to find sneaky numbers

--- sneakynums.cpp
+++ sneakynums.cpp
@@ -1,1 +1,17 @@
 
+class Solution {
+    public:
+        vector<int> getSneakyNumbers(vector<int>& nums) {
+            vector<int> sneaked;
+            unordered_map<int,int> mpp;
+            for(int i=0;i<nums.size();i++){
+                mpp[nums[i]]++;
+            }
+            for(auto it:mpp){
+                if(it.second==2){
+                    sneaked.push_back(it.first);
+                }
+            }
+            return sneaked;
+        }
+    };
