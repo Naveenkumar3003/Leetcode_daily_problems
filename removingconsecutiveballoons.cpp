f4913f543f3d234d169271ec7a904ba77b9c9fbf
Naveenkumar3003 <naveengoogl2006@gmail.com>
2025-11-03 16:26:22 +0530

    Add Solution class with minCost method
    Implement minCost function to calculate minimum cost of removing consecutive balloons.

--- removingconsecutiveballoons.cpp
+++ removingconsecutiveballoons.cpp
@@ -1,1 +1,18 @@
-
+class Solution {
+public:
+    int minCost(string colors, vector<int>& neededTime) {
+        int time=0;
+        for(int i=1;i<colors.length();i++){
+            if(colors[i]==colors[i-1]){
+                if(neededTime[i]<=neededTime[i-1]){
+                    time+=neededTime[i];
+                    neededTime[i]=neededTime[i-1];
+                }
+                else{
+                    time+=neededTime[i-1];
+                }
+            }
+        }
+        return time;
+    }
+};
