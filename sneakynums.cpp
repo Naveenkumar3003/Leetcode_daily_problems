7fb79fbdb17736eca2e4f97d251a0c830aa72583
Naveenkumar N <naveengoogl2006@gmail.com>
2025-12-18 23:35:06 +0530

    Update sneakynums.cpp

--- sneakynums.cpp
+++ sneakynums.cpp
@@ -7,7 +7,7 @@ class Solution {
             for(int i=0;i<nums.size();i++){
                 mpp[nums[i]]++;
             }  
-            for(auto it:mpp){
+            for(auto it:mpp){  
                 if(it.second==2){
                     sneaked.push_back(it.first);
                 }
