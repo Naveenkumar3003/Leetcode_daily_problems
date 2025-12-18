8faf631e36843a12a36458a69b9dac27f7f6c26a
Naveenkumar N <naveengoogl2006@gmail.com>
2025-12-18 23:34:48 +0530

    Update sneakynums.cpp

--- sneakynums.cpp
+++ sneakynums.cpp
@@ -6,7 +6,7 @@ class Solution {
             unordered_map<int,int> mpp;
             for(int i=0;i<nums.size();i++){
                 mpp[nums[i]]++;
-            }
+            }  
             for(auto it:mpp){
                 if(it.second==2){
                     sneaked.push_back(it.first);
